7bcf2e422836549054bddb046be07aa3f45a0b4e
sasalin777 <candysasame@gmail.com>
2022-05-16 08:29:10 -0700

    upload

---
+++ ContactList/ContactList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Li Tzu Lin on 2022-05-16.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
